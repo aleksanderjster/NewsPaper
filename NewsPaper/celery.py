679536368ecb09fe2c03696b97f8a1838fce822c
aer <a.ermolaev.ee@gmail.com>
2023-08-20 15:00:11 +0200

    celery initiated

---
+++ NewsPaper/celery.py
@@ -0,0 +1,15 @@
+import os
+from celery import Celery
+ 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
+ 
+app = Celery('NewsPaper')
+app.config_from_object('django.conf:settings', namespace = 'CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule ={
+    'action_every_30_seconds':{
+        'task': ''
+    }
+}

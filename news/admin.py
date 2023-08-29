a213592661d2171da6513e1376c9e1f00313af5a
aer <a.ermolaev.ee@gmail.com>
2023-08-29 21:32:21 +0200

    customization admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post, Category
 
 # Register your models here.
+
+def reset_rating(ModelAdmin, request, queryset):
+    queryset.update(rating=0)
+    reset_rating.short_description = 'reset rating'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'preview', 'author' ,'publication_date', 'rating']
+    list_filter = ('author', 'publication_date')
+    search_fields = ('title', 'content')
+    actions = [reset_rating]
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
